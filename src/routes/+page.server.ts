ac4f029622d3412e5e5478a0922d0e65138bcb31
Devin Droddy <devin.droddy@gmail.com>
2024-10-19 13:16:59 -0400

    limit length of articles list

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,5 +3,6 @@ import type { Article } from '$lib/types';
 export async function load({ fetch }: { fetch: (url: string) => Promise<Response> }) {
   const response = await fetch('api/articles');
   const articles: Article[] = await response.json();
+  articles.length = Math.min(articles.length, 8);
   return { articles };
 }
