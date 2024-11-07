cd5f53d739d96e807a42fb6cecb18f6dee87603a
Devin Droddy <devin.droddy@gmail.com>
2024-11-07 13:49:38 -0500

    add margin to articles

--- src/routes/articles/[slug]/+page.svelte
+++ src/routes/articles/[slug]/+page.svelte
@@ -14,19 +14,27 @@
   <meta property="og:title" content={data.meta.title} />
 </svelte:head>
 
-<hgroup>
-  <h1 style="margin-bottom: 0">{data.meta.title}</h1>
-  <p style="margin-top: 0">{data.meta.date}</p>
+<div class="article-body">
+  <hgroup>
+    <h1 style="margin-bottom: 0">{data.meta.title}</h1>
+    <p style="margin-top: 0">{data.meta.date}</p>
 
-  <div class="row">
-    {#each data.meta.categories as category}
-      <p style="margin-top: 0">
-        <a href="/articles?tags=[&quot;{category}&quot;]">&num;{category}</a>&nbsp;
-      </p>
-    {/each}
-  </div>
-</hgroup>
+    <div class="row">
+      {#each data.meta.categories as category}
+        <p style="margin-top: 0">
+          <a href="/articles?tags=[&quot;{category}&quot;]">&num;{category}</a>&nbsp;
+        </p>
+      {/each}
+    </div>
+  </hgroup>
 
-<div>
   <data.content />
 </div>
+
+<style>
+  .article-body {
+    margin-left: 10px;
+    margin-right: 10px;
+    margin-bottom: 10px;
+  }
+</style>
