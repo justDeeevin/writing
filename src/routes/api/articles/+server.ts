28686e1ff937367f67d1768c1cc27cb1c3293d14
Devin Droddy <devin.droddy@gmail.com>
2024-11-13 11:16:54 -0500

    re-add articles API and update CORS rules
    I forgot that my homepage needs to get the list X_X

---
+++ src/routes/api/articles/+server.ts
@@ -0,0 +1,20 @@
+import { getArticles } from '$lib';
+
+export async function GET(): Promise<Response> {
+  return new Response(JSON.stringify(getArticles()), {
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*.justdeeevin.dev',
+      'Access-Control-Allow-Methods': 'GET'
+    }
+  });
+}
+
+export async function OPTIONS(): Promise<Response> {
+  return new Response(null, {
+    headers: {
+      'Access-Control-Allow-Origin': '*.justdeeevin.dev',
+      'Access-Control-Allow-Methods': 'GET'
+    }
+  });
+}
