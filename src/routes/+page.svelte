93ff0612dfb3fc5ba913e56df2d59270c36edbae
not-nullptr <needhelpwithrift@gmail.com>
2024-11-26 00:05:54 +0000

    feat: slightly nicer homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,8 +8,8 @@
   <title>devin's writing</title>
 </svelte:head>
 
-<h1>writing</h1>
-<p>
+<h1 style="margin-bottom: 32px; margin-top: 48px; font-size: 3rem; line-height: 4rem;">writing</h1>
+<p style="margin-bottom: 32px;">
   i write english about as well as i write rust or typescript. sometimes i want to publish my
   writing. that's what this is for.
 </p>
